dd70cf175d3cbb4bcd86eb768014de99df3150c3
Sylvain Kalache <sylvain@kalache.fr>
2023-05-08 14:44:26 -0400

    Adding Python app sample files

---
+++ my-app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template
+import subprocess
+import gunicorn
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return 'Hello, TheNewStack users!\n'
+
+    
